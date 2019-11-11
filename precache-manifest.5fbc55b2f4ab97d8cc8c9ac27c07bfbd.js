bc4723c6a27a23e2e76c2fd6f64d9565d3f79818
SarAbigail <samihimezu@gmail.com>
2019-11-11 14:31:42 -0500

    Updates

---
+++ precache-manifest.5fbc55b2f4ab97d8cc8c9ac27c07bfbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083f71467ee643ac39c0190416146f4b",
+    "url": "/SarAbigail/LIM010-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "1322c587233bef6a2cff",
+    "url": "/SarAbigail/LIM010-fe-burger-queen/static/css/main.3f6f7120.chunk.css"
+  },
+  {
+    "revision": "9f0b9cc7edb91d69d6e3",
+    "url": "/SarAbigail/LIM010-fe-burger-queen/static/js/2.e7ce5dc1.chunk.js"
+  },
+  {
+    "revision": "1322c587233bef6a2cff",
+    "url": "/SarAbigail/LIM010-fe-burger-queen/static/js/main.f6ccc905.chunk.js"
+  },
+  {
+    "revision": "33887146ff751af3b89e",
+    "url": "/SarAbigail/LIM010-fe-burger-queen/static/js/runtime-main.8509e425.js"
+  }
+]);
